5f6185521916f7e2ce1bffe8032147be75cded35
Nick <heckmancareer@vivaldi.net>
2023-12-08 22:03:08 -0500

    Scroll functionality

--- src/modules/portfolio/pages/portfolio-main-page/portfolio-main-page.component.ts
+++ src/modules/portfolio/pages/portfolio-main-page/portfolio-main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, ElementRef, QueryList, AfterViewInit } from '@angular/core';
 import { PORTFOLIO_DATA } from './main-page-data';
 
 @Component({
@@ -7,6 +7,26 @@ import { PORTFOLIO_DATA } from './main-page-data';
   styleUrl: './portfolio-main-page.component.sass'
 })
 export class PortfolioMainPageComponent {
+  @ViewChildren('scrollTarget') elements!: QueryList<ElementRef>
+  private elementMap: Record<string, ElementRef> = {}
+  // This should be equal to the pixel height of the navbar
+  private readonly scrollOffset = 60
   data: any = PORTFOLIO_DATA
   relativeImagePath: string = '../../../../assets/'
+
+  ngAfterViewInit() {
+    this.elements.forEach((element: ElementRef) => {
+      this.elementMap[element.nativeElement.id] = element
+    })
+  }
+
+  scrollToElementWithOffset(targetElement: string): void {
+    const elementRef = this.elementMap[targetElement]
+    if(elementRef) {
+      const elementRect = elementRef.nativeElement.getBoundingClientRect()
+      const absoluteElementTop = elementRect.top + window.scrollY
+      const scrollTop = absoluteElementTop - this.scrollOffset
+      window.scrollTo({top: scrollTop, behavior: 'smooth'})
+    }
+  }
 }
